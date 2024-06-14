324c95e41fe19e77430135298ebbf9b66943ca61
Mr-Vimal <mrvimal0501@gmail.com>
2024-06-14 07:31:27 +0530

    render-server-Update 1.5

--- Client/src/Admin/UserAdd/UserAdd.js
+++ Client/src/Admin/UserAdd/UserAdd.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 
-export default function UserFetch({ onClose }) {
+export default function UserAdd({ onClose }) {
     const [FirstName, setFirstName] = useState('');
     const [LastName, setLastName] = useState('');
     const [Email, setEmail] = useState('');
